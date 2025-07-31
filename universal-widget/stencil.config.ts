a78de35f32d402b2ff209d9c7c8b6358a04a12c1
soumyabera-aisera <soumya.bera@aisera.com>
2025-07-31 09:30:26 +0530

    updated build commands

--- universal-widget/stencil.config.ts
+++ universal-widget/stencil.config.ts
@@ -4,23 +4,8 @@ export const config: Config = {
   namespace: 'universal-widget',
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
       type: 'dist-custom-elements',
-      customElementsExportBehavior: 'auto-define-custom-elements',
-      externalRuntime: false,
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
+      customElementsExportBehavior: 'single-export-module',
     },
   ],
-  testing: {
-    browserHeadless: "shell",
-  },
 };
